301db1abfdb3b9d2e5ff298ce3bff53a2429fec0
EmersonBottega <emerbtts@hotmail.com>
2025-12-11 20:53:49 -0300

    feat: add restaurant tests and required name

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,7 +1,65 @@
 require "test_helper"
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @restaurant = restaurants(:one)
+  end
+
+  test "should get index" do
+    get restaurants_url, as: :json
+    assert_response :success
+  end
+
+  test "should show restaurant" do
+    get restaurant_url(@restaurant), as: :json
+    assert_response :success
+
+    json = JSON.parse(response.body)
+    assert_equal @restaurant.name, json["name"]
+  end
+
+  test "should create restaurant" do
+    assert_difference("Restaurant.count") do
+      post restaurants_url, params: {
+        restaurant: { name: "New Restaurant" }
+      }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should not create restaurant without name" do
+    post restaurants_url, params: {
+      restaurant: { name: "" }
+    }, as: :json
+
+    assert_response :unprocessable_content
+  end
+
+  test "should update restaurant" do
+    put restaurant_url(@restaurant), params: {
+      restaurant: { name: "Updated Name" }
+    }, as: :json
+
+    assert_response :success
+
+    @restaurant.reload
+    assert_equal "Updated Name", @restaurant.name
+  end
+
+  test "should not update restaurant with invalid data" do
+    put restaurant_url(@restaurant), params: {
+      restaurant: { name: "" }
+    }, as: :json
+
+    assert_response :unprocessable_content
+  end
+
+  test "should destroy restaurant" do
+    assert_difference("Restaurant.count", -1) do
+      delete restaurant_url(@restaurant), as: :json
+    end
+
+    assert_response :no_content
+  end
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    restaurant = Restaurant.new(name: "Poppo's Restaurant")
+    assert restaurant.valid?
+  end
+
+  test "is invalid without a name" do
+    restaurant = Restaurant.new(name: nil)
+    assert_not restaurant.valid?
+    assert_includes restaurant.errors[:name], "can't be blank"
+  end
+
+  test "is invalid with duplicated name" do
+    Restaurant.create!(name: "Poppo's Cafe")
+    duplicate = Restaurant.new(name: "Poppo's Cafe")
+
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:name], "has already been taken"
+  end
 end
